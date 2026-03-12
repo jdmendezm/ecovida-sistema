9b254b481d6e5e54922c63a0cfb78ff603a9ef90
jdmendezm <jdmendezm@sanmateo.edu.co>
2026-03-11 23:32:07 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 app = Flask(__name__)
 
+# Cargar datos
+data = pd.read_csv("../dataset/consumo_energetico.csv")
+
+# Estadísticas
+consumo_promedio = round(data["consumo_kwh"].mean(),2)
+consumo_max = data["consumo_kwh"].max()
+consumo_min = data["consumo_kwh"].min()
+
+# Modelo IA
+X = np.array(range(len(data))).reshape(-1,1)
+y = data["consumo_kwh"]
+
+modelo = LinearRegression()
+modelo.fit(X,y)
+
+prediccion = round(modelo.predict([[12]])[0],2)
+
+# Alertas
+limite = 160
+alertas = []
+
+for consumo in data["consumo_kwh"]:
+    if consumo > limite:
+        alertas.append(consumo)
+
 @app.route("/")
-def inicio():
+def index():
+    
+    return render_template(
+        "index.html",
+        promedio=consumo_promedio,
+        maximo=consumo_max,
+        minimo=consumo_min,
+        prediccion=prediccion,
+        alertas=alertas
+    )
 
-    data = pd.read_csv("../dataset/consumo_energetico.csv")
-
-    return data.to_json()
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
